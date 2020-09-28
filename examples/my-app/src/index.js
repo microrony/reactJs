60f08c65ed4edef97261ae00f001e28dc4791796
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-28 08:25:54 +0600

    functional component

--- examples/my-app/src/index.js
+++ examples/my-app/src/index.js
@@ -1,16 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Greet from './components/greet';
 
-const root = document.getElementById('root');
-
-function MyApp() {
-  return (
-    <ul>
-      <li>list one</li>
-      <li>list two</li>
-      <li>list three</li>
-    </ul>
-  );
-}
-
-ReactDom.render(<MyApp />, root);
+ReactDom.render(<Greet />, document.getElementById('root'));
