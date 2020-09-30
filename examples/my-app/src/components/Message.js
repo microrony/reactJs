0529212ccbea761b1b8373465799820ae63af4bf
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-30 22:12:50 +0600

    add onClick event with state

--- examples/my-app/src/components/Message.js
+++ examples/my-app/src/components/Message.js
@@ -7,8 +7,21 @@ class Message extends Component {
       message: 'Welcome visitor'
     }
   }
+
+  changeMessage() {
+    this.setState({
+      message: 'Thank you for subscribing'
+    })
+  }
+
+
   render() {
-    return <h1>{this.state.message}</h1>;
+    return (
+      <div>
+      <h1>{this.state.message}</h1>
+      <button onClick={() => this.changeMessage()}>Subscribe</button>
+      </div>
+    )
   }
 }
 
