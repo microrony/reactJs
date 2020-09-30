d437dd9f765b2c9f3261391760f0610fe35dde65
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-30 22:22:35 +0600

    update

--- examples/my-app/src/components/Message.js
+++ examples/my-app/src/components/Message.js
@@ -4,22 +4,21 @@ class Message extends Component {
   constructor() {
     super()
     this.state = {
-      message: 'Welcome visitor'
+      message: 'Welcome Visitor'
     }
   }
 
-  changeMessage() {
+  messageChanged() {
     this.setState({
-      message: 'Thank you for subscribing'
+      message: 'Thanks for subscribing'
     })
   }
-
 
   render() {
     return (
       <div>
-      <h1>{this.state.message}</h1>
-      <button onClick={() => this.changeMessage()}>Subscribe</button>
+         <h1>{this.state.message}</h1>
+         <button onClick={() => this.messageChanged()}>Subscribe</button>
       </div>
     )
   }
