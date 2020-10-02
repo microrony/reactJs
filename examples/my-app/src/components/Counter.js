a47605a6172ef6dec035a3fe43d99aa53c578112
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-02 19:08:18 +0600

    update

--- examples/my-app/src/components/Counter.js
+++ examples/my-app/src/components/Counter.js
@@ -3,27 +3,19 @@ import React, { Component } from 'react';
 class Counter extends Component {
   constructor() {
     super()
-  
+
     this.state = {
-       count: 0
+      count: 0
     }
   }
-  
 
   increment() {
     // this.setState({
     //   count: this.state.count + 1
-    // },
-    // () => {
-    //   console.log('callback value', this.state.count)
     // })
-    // console.log(this.state.count)
-    this.setState(prevState => ({
-      count: prevState.count + 1
-    }))
   }
 
-  incrementFive () {
+  incrementFive() {
     this.increment()
     this.increment()
     this.increment()
@@ -34,8 +26,8 @@ class Counter extends Component {
   render() {
     return (
       <div>
-      <div>count - {this.state.count}</div>
-      <button onClick={() => this.incrementFive()}>Increment</button>
+        <div>count - {this.state.count}</div>
+        <button onClick={() => this.incrementFive()}>Increment</button>
       </div>
     )
   }
