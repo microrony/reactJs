595d49396a3119d7f59efb6882581a187d7c8361
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-03 22:47:13 +0600

    Binding Event Handlers

--- examples/my-app/src/App.js
+++ examples/my-app/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import FunctionClick from './components/FunctionClick'
+import EventBind from './components/EventBind'
 
 function App() {
   return (
     <div className = 'App'>
-      <FunctionClick />
+      <EventBind />
     </div>
   );
 }
---
+++ examples/my-app/src/components/EventBind.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class EventBind extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      message: 'Hello'
+    }
+
+  }
+
+  clickHandler = () => {
+    this.setState({
+      message: 'Goodbye'
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.message}</div>
+        <button onClick={this.clickHandler}>Click</button>
+      </div>
+    )
+  }
+}
+
+export default EventBind;
