9be9500cfa809f27fc1a1292083ba71b1d902f79
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-04 10:54:16 +0600

    if/else statement

---
+++ examples/my-app/src/components/UserGreeting.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class UserGreeting extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      isLoggedIn: true
+    }
+  }
+
+  render() {
+    if (this.state.isLoggedIn) {
+      return <div>Welcome Mahabubur</div>
+    } else {
+      return <div>Welcome Guest</div>
+    }
+  }
+}
+
+export default UserGreeting;
