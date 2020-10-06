f6523e596c12e358b429eec6064d82a95f38ea1c
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-06 12:19:37 +0600

    update

--- examples/my-app/src/components/Person.js
+++ examples/my-app/src/components/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Person({person, key}) {
+function Person({person}) {
   return (
     <div>
       <h2>I am {person.name}. I am {person.age} years old. I know {person.skill}</h2>
