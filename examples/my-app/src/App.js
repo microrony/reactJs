a93e251585d492d2cae4b890cae6860bb9001737
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-06 20:53:16 +0600

    css module in react

--- examples/my-app/src/App.js
+++ examples/my-app/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Inline from './components/Inline'
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className = 'App'>
-      <Inline primary={true} />
+      <h2 className= 'error'>Error</h2>
+      <h2 className={styles.success}>Success</h2>
     </div>
   );
 }
