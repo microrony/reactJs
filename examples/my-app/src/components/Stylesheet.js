ba47fcad4b407109ffa00bf2fa1df668e75aceee
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-06 18:08:26 +0600

    update

--- examples/my-app/src/components/Stylesheet.js
+++ examples/my-app/src/components/Stylesheet.js
@@ -5,7 +5,7 @@ function Stylesheet(props) {
   let className = props.primary ? 'primary' : '';
   return (
     <div>
-      <h2 className={className}>Stylesheet</h2>
+      <h2 className={`${className} font-xl`}>Stylesheet</h2>
     </div>
   )
 }
