58cd37235e75ad3519c5d33312367d7db44c4059
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-06 10:59:56 +0600

    update

--- examples/my-app/src/components/NameList.js
+++ examples/my-app/src/components/NameList.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Name from './Name';
 
 function NameList() {
   const names = ['Mahabubur', 'Sattar', 'Raju'];
-  const nameList =  names.map(name => <Name name={name} />)
+  const nameList = names.map(name => <h2>{name}</h2>);
   return <div>{nameList}</div>
 }
 
