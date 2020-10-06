fdeaff31e16ab30152c78008397123fe6dfb3555
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-06 17:30:30 +0600

    update

--- examples/my-app/src/components/NameList.js
+++ examples/my-app/src/components/NameList.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 function NameList() {
   const names = ['Mahabubur', 'Sattar', 'Raju'];
-  const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>);
-  return <div>{nameList}</div>
+  const nameList = names.map((name, index) => <h2 key={index}>{name}</h2>)
+  return (
+    <div>{nameList}</div>
+  )
 }
 
 export default NameList;
