ab5447fcb4c053b4ce83188b3eea0635ac316c64
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-06 17:25:39 +0600

    index as a key

--- examples/my-app/src/components/NameList.js
+++ examples/my-app/src/components/NameList.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import Person from './Person'
 
 function NameList() {
-  const persons = [
-    {
-      id: 1,
-      name: 'Mahabubur',
-      age: 20,
-      skill: 'Programming'
-    },
-    {
-      id: 2,
-      name: 'Sattar',
-      age: 25,
-      skill: 'Dopper'
-    },
-    {
-      id: 3,
-      name: 'Raju',
-      age: 20,
-      skill: 'Fishing'
-    }
-  ];
-  const personList = persons.map(person => <Person key={person.id} person={person} />);
-  return <div>{personList}</div>
+  const names = ['Mahabubur', 'Sattar', 'Raju'];
+  const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>);
+  return <div>{nameList}</div>
 }
 
 export default NameList;
