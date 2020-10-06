f6b4fd3581d36b2e43d2adaabec0f30afebdeede
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-06 18:16:49 +0600

    update

---
+++ examples/my-app/src/components/Inline.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Inline() {
+  const heading = {
+    fontSize: '2rem',
+    color: 'orange'
+  }
+  return (
+    <div>
+      <h2 style={heading}>Inline</h2>
+    </div>
+  )
+}
+
+export default Inline
