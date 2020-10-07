efce349703298ff8f78a19d75963a9146d635065
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-07 07:58:33 +0600

    update

--- examples/my-app/src/components/Form.js
+++ examples/my-app/src/components/Form.js
@@ -6,7 +6,9 @@ export class Form extends Component {
     super(props)
   
     this.state = {
-       username: ''
+       username: '',
+       comments: '',
+       topic: 'react'
     }
   }
 
@@ -15,14 +17,39 @@ export class Form extends Component {
       username: event.target.value
     })
   }
-  
+
+  handleCommentsChange = (event) => {
+    this.setState({
+      comments: event.target.value
+    })
+  }
+
+  handleTopicChange = (event) => {
+    this.setState({
+      topic: event.target.value
+    })
+  }
 
   render() {
     return (
+      <form>
       <div>
         <label>Username</label>
         <input type= 'text' value={this.state.username} onChange={this.handleUsernameChange}/>
       </div>
+      <div>
+        <label>Comments</label>
+        <textarea value={this.state.comments} onChange={this.handleCommentsChange}></textarea>
+      </div>
+      <div>
+        <label>Topic</label>
+        <select value={this.state.topic} onChange={this.handleTopicChange}>
+        <option value='react'>React</option>
+        <option value='angular'>Angular</option>
+        <option value='vue'>Vue</option>
+        </select>
+      </div>
+      </form>
     )
   }
 }
