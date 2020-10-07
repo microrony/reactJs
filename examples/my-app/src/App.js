7681d351286e1414e14452fa8e2f9f289bcd3861
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-07 07:39:36 +0600

    update

--- examples/my-app/src/App.js
+++ examples/my-app/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Inline from './components/Inline'
+import Form from './components/Form'
 
 function App() {
   return (
     <div className = 'App'>
-      <Inline />
+      <Form />
     </div>
   );
 }
