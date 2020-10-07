7e09a45930c9c5caba1c377e23df8620d9d9dda5
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-07 06:34:23 +0600

    update

--- examples/my-app/src/App.js
+++ examples/my-app/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 import Inline from './components/Inline'
-import './appStyles.css'
-import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className = 'App'>
-      <h2 className= 'error'>Error</h2>
-      <h2 className={styles.success}>Success</h2>
+      <Inline />
     </div>
   );
 }
