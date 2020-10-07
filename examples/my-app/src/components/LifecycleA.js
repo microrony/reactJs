c62b066193b8bd98c0f72f1e0b7447f08b7e073c
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-07 17:10:19 +0600

    lifecycle methods mounting

--- examples/my-app/src/components/LifecycleA.js
+++ examples/my-app/src/components/LifecycleA.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
-import LifecycleB from './LifecycleB';
 
 class LifecycleA extends Component {
   constructor(props) {
     super(props);
-    console.log('LifecycleA constructor');
+
     this.state = {
-      name: 'mahabubur',
+      name: 'Mahabubur',
     };
+    console.log('constructor');
   }
 
   static getDerivedStateFromProps(props, state) {
-    console.log('LifecycleA getDerivedStateFromProps');
+    console.log('getDerivedStateFromProps');
     return null;
   }
 
   componentDidMount() {
-    console.log('LifecycleA componentDidMount');
+    console.log('componenetDidMount');
   }
 
   render() {
-    console.log('LifecycleA render');
-    return (
-      <div>
-        <div>Lifecycle A</div>
-        <LifecycleB />
-      </div>
-    );
+    console.log('render');
+    return <div>Lifecycle A</div>;
   }
 }
 
