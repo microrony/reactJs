a109b00553b526a9eceab038163a8eafc2b37e1e
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-08 18:19:27 +0600

    update

--- examples/my-app/src/components/LifecycleA.js
+++ examples/my-app/src/components/LifecycleA.js
@@ -8,23 +8,44 @@ class LifecycleA extends Component {
     this.state = {
       name: 'Mahabubur',
     }
-    console.log('constructor')
+    console.log('LifecycleA constructor')
   }
 
   static getDerivedStateFromProps(props, state) {
-    console.log('getDerivedStateFromProps')
+    console.log('LifecycleA getDerivedStateFromProps')
     return null
   }
 
   componentDidMount() {
-    console.log('componenetDidMount')
+    console.log('LifecycleA componenetDidMount')
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('LifecycleA shouldComponentUpdate')
+    return true
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('LifecycleA getSnapshotBeforeUpdate')
+    return null
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('LifecycleA componentDidUpdate')
+  }
+
+  changeState = () => {
+    this.setState({
+      name: 'Rony',
+    })
   }
 
   render() {
-    console.log('render')
+    console.log('LifecycleA render')
     return (
       <div>
         <div>Lifecycle A</div>
+        <button onClick={this.changeState}>Change State</button>
         <LifecycleB />
       </div>
     )
