bbbd15a28ee92741b906160d63677151cf9c290a
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-09 17:22:22 +0600

    update

--- examples/my-app/src/App.js
+++ examples/my-app/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
-import FragmentDemo from './components/FragmentDemo'
+import Table from './components/Table'
 
 function App() {
   return (
     <div className="App">
-      <FragmentDemo />
+      <Table />
     </div>
   )
 }
