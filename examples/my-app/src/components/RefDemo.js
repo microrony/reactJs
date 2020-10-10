5536146a1512b46341923f0b12f1ad97026f787d
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-10 22:02:48 +0600

    update

--- examples/my-app/src/components/RefDemo.js
+++ examples/my-app/src/components/RefDemo.js
@@ -5,21 +5,26 @@ class RefDemo extends Component {
     super(props)
 
     this.inputRef = React.createRef()
+    this.cbRef = null
+    this.setCbRef = element => {
+      this.cbRef = element
+    }
   }
 
   componentDidMount() {
-    console.log(this.inputRef)
-    this.inputRef.current.focus()
+    if (this.cbRef) {
+      this.cbRef.focus()
+    }
   }
 
   clickHandler = () => {
-    alert(this.inputRef.current.value)
+    alert(this.cbRef.value)
   }
 
   render() {
     return (
       <div>
-        <input type="text" ref={this.inputRef} />
+        <input type="text" ref={this.setCbRef} />
         <button onClick={this.clickHandler}>Click</button>
       </div>
     )
