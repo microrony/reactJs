f44f0705f3f3f9994ce1001dbda513846301fb03
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-10 23:40:43 +0600

    ref modern way

--- examples/my-app/src/components/RefDemo.js
+++ examples/my-app/src/components/RefDemo.js
@@ -5,26 +5,20 @@ class RefDemo extends Component {
     super(props)
 
     this.inputRef = React.createRef()
-    this.cbRef = null
-    this.setCbRef = element => {
-      this.cbRef = element
-    }
   }
 
   componentDidMount() {
-    if (this.cbRef) {
-      this.cbRef.focus()
-    }
+    this.inputRef.current.focus()
   }
 
   clickHandler = () => {
-    alert(this.cbRef.value)
+    alert(this.inputRef.current.value)
   }
 
   render() {
     return (
       <div>
-        <input type="text" ref={this.setCbRef} />
+        <input type="text" ref={this.inputRef} />
         <button onClick={this.clickHandler}>Click</button>
       </div>
     )
