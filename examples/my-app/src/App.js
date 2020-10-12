387f56663ca0b02c814c969b96e29f74e0a12662
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-12 21:08:46 +0600

    update

--- examples/my-app/src/App.js
+++ examples/my-app/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './App.css'
-import PortalDemo from './components/PortalDemo'
+import Hero from './components/Hero'
+import ErrorBoundary from './components/ErrorBoundary'
 
 function App() {
   return (
     <div className="App">
-      <PortalDemo />
+      <ErrorBoundary>
+        <Hero heroName="Batman" />
+        <Hero heroName="Super Man" />
+        <Hero heroName="Joker" />
+      </ErrorBoundary>
     </div>
   )
 }
