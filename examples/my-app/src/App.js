fd00e583dd2c135789dfbb990c55d9a7de015d6a
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-13 16:53:55 +0600

    update

--- examples/my-app/src/App.js
+++ examples/my-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import ClickCounterTwo from './components/ClickCounterTwo'
 import HoverCounterTwo from './components/HoverCounterTwo'
+import User from './components/User'
 
 function App() {
   return (
     <div className="App">
       <ClickCounterTwo />
       <HoverCounterTwo />
+      <User render={isLoggedIn => (isLoggedIn ? 'Mahabubur' : 'Guest')} />
     </div>
   )
 }
