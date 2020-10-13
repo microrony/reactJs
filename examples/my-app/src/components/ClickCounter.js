7d780f2853e76db2968e1f28a1dc26d08e861e3e
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-13 12:42:39 +0600

    update

--- examples/my-app/src/components/ClickCounter.js
+++ examples/my-app/src/components/ClickCounter.js
@@ -4,7 +4,11 @@ import withCounter from './withCounter'
 class ClickCounter extends Component {
   render() {
     const { count, incrementCount } = this.props
-    return <button onClick={incrementCount}>Clicked {count} Times</button>
+    return (
+      <button onClick={incrementCount}>
+        {this.props.name} Clicked {count} Times
+      </button>
+    )
   }
 }
 
