1b8e5995adcfc33b34a152faef6f7436b6200abf
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-13 17:38:34 +0600

    update

--- examples/my-app/src/App.js
+++ examples/my-app/src/App.js
@@ -2,14 +2,21 @@ import React from 'react'
 import './App.css'
 import ClickCounterTwo from './components/ClickCounterTwo'
 import HoverCounterTwo from './components/HoverCounterTwo'
-import User from './components/User'
+import CounterTwo from './components/CounterTwo'
 
 function App() {
   return (
     <div className="App">
-      <ClickCounterTwo />
-      <HoverCounterTwo />
-      <User render={isLoggedIn => (isLoggedIn ? 'Mahabubur' : 'Guest')} />
+      <CounterTwo>
+        {(count, incrementCount) => (
+          <ClickCounterTwo count={count} incrementCount={incrementCount} />
+        )}
+      </CounterTwo>
+      <CounterTwo>
+        {(count, incrementCount) => (
+          <HoverCounterTwo count={count} incrementCount={incrementCount} />
+        )}
+      </CounterTwo>
     </div>
   )
 }
--- examples/my-app/src/components/HoverCounterTwo.js
+++ examples/my-app/src/components/HoverCounterTwo.js
@@ -1,23 +1,9 @@
 import React, { Component } from 'react'
 
 class ClickCounterTwo extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      count: 0,
-    }
-  }
-
-  incrementCount = () => {
-    this.setState(prevState => {
-      return { count: prevState.count + 1 }
-    })
-  }
-
   render() {
-    const { count } = this.state
-    return <h2 onMouseOver={this.incrementCount}>Hovered {count} Times</h2>
+    const { count, incrementCount } = this.props
+    return <h2 onMouseOver={incrementCount}>Hovered {count} Times</h2>
   }
 }
 
