b5594697e45e1c2d976ac60996898bdc00cc0c0d
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-20 12:54:00 +0600

    update

--- examples/react-hooks/src/App.js
+++ examples/react-hooks/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import HookMouse from './components/HookMouse';
+import MouseContainer from './components/MouseContainer';
 
 function App() {
   return (
     <div className="App">
-      <HookMouse />
+      <MouseContainer />
     </div>
   );
 }
