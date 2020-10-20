a98c54c13a2deccc0c3024c01bd2b31596d5ae69
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-20 12:45:32 +0600

    functional component show mouse positions

--- examples/react-hooks/src/App.js
+++ examples/react-hooks/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
 
 function App() {
   return (
     <div className="App">
-      <ClassMouse />
+      <HookMouse />
     </div>
   );
 }
---
+++ examples/react-hooks/src/components/HookMouse.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+function HookMouse() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const logMousePosition = e => {
+    console.log('Mouse event');
+    setX(e.clientX);
+    setY(e.clientY);
+  };
+
+  useEffect(() => {
+    console.log('useEffect Updated');
+    window.addEventListener('mousemove', logMousePosition);
+  }, []);
+
+  return (
+    <div>
+      Hook - X - {x} Y - {y}
+    </div>
+  );
+}
+
+export default HookMouse;
