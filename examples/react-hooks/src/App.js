5c0dbaf804b70b9c7463b1ee89c45bb68002a92b
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-20 16:41:51 +0600

    interval with functional component

--- examples/react-hooks/src/App.js
+++ examples/react-hooks/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import IntervalClassCounter from './components/IntervalClassCounter';
+import IntervalHookCounter from './components/IntervalHookCounter';
 
 function App() {
   return (
     <div className="App">
-      <IntervalClassCounter />
+      <IntervalHookCounter />
     </div>
   );
 }
---
+++ examples/react-hooks/src/components/IntervalHookCounter.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+function IntervalHookCounter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(tick, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const tick = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  return <h1>{count}</h1>;
+}
+
+export default IntervalHookCounter;
