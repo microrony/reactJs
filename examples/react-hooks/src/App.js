67379b198884cda4a151e5c225b11a0ffc944fa9
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-23 17:05:58 +0600

    update

--- examples/react-hooks/src/App.js
+++ examples/react-hooks/src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './App.css';
 import ComponentC from './components/ComponentC';
 
+export const UserContext = React.createContext();
+export const UserNameContext = React.createContext();
+
 function App() {
   return (
     <div className="App">
-      <ComponentC />
+      <UserContext.Provider value={'Mahabubur'}>
+        <UserNameContext.Provider value={'microrony'}>
+          <ComponentC />
+        </UserNameContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
--- examples/react-hooks/src/components/ComponentF.js
+++ examples/react-hooks/src/components/ComponentF.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { UserContext, UserNameContext } from '../App';
 
 function ComponentC() {
-  return <div>Component F</div>;
+  return (
+    <UserContext.Consumer>
+      {user => {
+        return (
+          <UserNameContext.Consumer>
+            {userName => (
+              <h2>
+                Name is {user} and username is {userName}
+              </h2>
+            )}
+          </UserNameContext.Consumer>
+        );
+      }}
+    </UserContext.Consumer>
+  );
 }
 
 export default ComponentC;
