a5e89cda22a1818cb9cb10d3d5c6d43e3100c3dc
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-23 12:23:05 +0600

    fatching data with useEffect hook

--- examples/react-hooks/src/components/DataFetching.js
+++ examples/react-hooks/src/components/DataFetching.js
@@ -2,27 +2,33 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function DataFetching() {
-  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+
+  const clickHandler = () => {
+    setIdFromButtonClick(id);
+  };
 
   useEffect(() => {
     axios
-      .get('https://jsonplaceholder.typicode.com/posts')
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then(res => {
         console.log(res);
-        setPosts(res.data);
+        setPost(res.data);
       })
       .catch(err => {
         console.log(err);
       });
-  }, []);
+  }, [idFromButtonClick]);
 
   return (
     <div>
-      <ol>
-        {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ol>
+      <input type="text" value={id} onChange={e => setId(e.target.value)} />
+      <button type="submit" onClick={clickHandler}>
+        Fetch Posts
+      </button>
+      <div>{post.title}</div>
     </div>
   );
 }
