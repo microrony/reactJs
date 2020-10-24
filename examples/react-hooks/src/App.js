7d549a991731d39d7cac06bc698e633ea902b562
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-24 18:27:13 +0600

    update

--- examples/react-hooks/src/App.js
+++ examples/react-hooks/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ComponentC from './components/ComponentC';
+import CounterOne from './components/CounterOne';
 
 export const UserContext = React.createContext();
 export const UserNameContext = React.createContext();
@@ -8,11 +8,7 @@ export const UserNameContext = React.createContext();
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={'Mahabubur'}>
-        <UserNameContext.Provider value={'microrony'}>
-          <ComponentC />
-        </UserNameContext.Provider>
-      </UserContext.Provider>
+      <CounterOne.js />
     </div>
   );
 }
