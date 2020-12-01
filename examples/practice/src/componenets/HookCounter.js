0f49976a0279f528483b72f54f375dc262e4dd78
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-12-01 12:48:05 +0600

    update

--- examples/practice/src/componenets/HookCounter.js
+++ examples/practice/src/componenets/HookCounter.js
@@ -2,9 +2,14 @@ import {useState} from 'react'
 
 const HookCounter = () => {
   const [count, setCount] = useState(0)
+
+  const clickHandler = () => {
+    setCount(prevState => prevState + 1)
+  }
+
   return (
     <div>
-      <button onClick={() => setCount(prevState => prevState + 1)}>Count {count}</button>
+      <button onClick={clickHandler}>Count {count}</button>
     </div>
   )
 }
