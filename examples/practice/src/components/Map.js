ec9bee256c8e6cb239cea7a7a884c308189d117c
Mahabubur Rahman <microronyy@gmail.com>
2020-12-12 09:08:19 +0600

    working on corona-tracker

--- examples/practice/src/components/Map.js
+++ examples/practice/src/components/Map.js
@@ -1,21 +1,15 @@
-import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from 'react-leaflet'
 
 const Map = () => {
-  const position = [24.63, 90.45];
+  const position = [24.63, 90.45]
   return (
-    <MapContainer
-      className="map"
-      center={position}
-      zoom={6}
-      scrollWheelZoom={false}
-      >
+    <MapContainer className="map" center={position} zoom={6}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
     </MapContainer>
-  );
+  )
 }
 
-export default Map;
+export default Map
