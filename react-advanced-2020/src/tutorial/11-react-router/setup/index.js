6ca7c2ea9e4658f4c72b99597294f1c2425e9078
Mahabubur Rahman <microronyy@gmail.com>
2021-01-05 08:54:09 +0600

    how to use router and route

--- react-advanced-2020/src/tutorial/11-react-router/setup/index.js
+++ react-advanced-2020/src/tutorial/11-react-router/setup/index.js
@@ -10,7 +10,19 @@ import Person from './Person';
 // navbar
 import Navbar from './Navbar';
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  return (
+    <Router>
+      <Route path='/' exact>
+        <Home/>
+      </Route>
+      <Route path='/about'>
+        <About/>
+      </Route>
+      <Route path='/people'>
+        <People/>
+      </Route>
+    </Router>
+  );
 };
 
 export default ReactRouterSetup;
